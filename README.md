304b295c35dc40bcca4ab744da22f9e1f4e7445b
Savita0807 <talktosavita@gmail.com>
2021-04-24 01:02:30 -0500

    adding py file

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Two-fer
+Two-fer or 2-fer is short for two for one. One for you and one for me.
+This is small python code that will return a string with the message forthe given name:
+    One for X, one for me.
+Where X is the given name.
+
+However, if the name is missing, return the string:
+    One for you, one for me. 
---
+++ main.py
@@ -0,0 +1,9 @@
+# This program display 2-Fer.
+# First program accepts the input from the user.
+User_inp = input("Enter any name: ")
+# Second program will display the 2-fer message.
+if User_inp != '':
+    print(f"One for {User_inp}, one for me.")   
+else:  
+# If the there is no imput provided it should display different messgae. 
+    print(f"One for you, one for me.")
